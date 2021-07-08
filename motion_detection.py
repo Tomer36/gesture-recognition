ee5f34c46081fb1ffadb73d533aa1fc6da48b383
Tomer36 <67845283+Tomer36@users.noreply.github.com>
2021-07-08 03:50:50 +0300

    Add files via upload

---
+++ motion_detection.py
@@ -0,0 +1,70 @@
+from imutils.video import VideoStream  # webCam Streaming
+import argparse  # read the arguments from the user from the argument line
+import datetime
+import imutils  # help us read video and imgs and work with
+import time
+import cv2  # opencv
+
+#  background subtracting
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the video file")
+ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+args = vars(ap.parse_args())
+
+# if no video file available start record from webcam
+if args.get("video", None) is None:
+    vs = VideoStream(src=0).start()
+    time.sleep(2.0)
+
+else:
+    vs = cv2.VideoCapture(args["video"])
+
+firstFrame = None
+
+while True:
+    frame = vs.read()
+    frame = frame if args.get("video", None) is None else frame[1]
+    text = "Unoccupied"
+
+    if frame is None:
+        break
+
+    frame = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    if firstFrame is None:
+        firstFrame = gray
+        continue
+
+    frameDelta = cv2.absdiff(firstFrame, gray)
+    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    for c in cnts:
+        if cv2.contourArea(c) < args["min_area"]:
+            continue
+
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        text = "Motion is Detected"
+
+    cv2.putText(frame, "Motion status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+    cv2.imshow("Video feed", frame)
+    cv2.imshow("Thresh", thresh)
+    cv2.imshow("Frame delta", frameDelta)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord('q'):
+        break
+
+vs.stop() if args.get("video", None) is None else vs.release()
+cv2.destroyAllWindows()
